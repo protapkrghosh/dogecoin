9fc13b8521bee923b996ec2dab39d3d0921026bf
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-11 00:27:33 +0600

    footer section

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,9 +1,20 @@
+import Container from '@/components/Sections/Container/Container';
+import footerLogo from '@/assets/banner/dogecoin-logo 1.png'
 import React from 'react';
 
 const Footer = () => {
   return (
     <div>
-      <h3 className='text-[#FFF]'>This is footer</h3>
+      <Container>
+        <div>
+          {/* first part  */}
+          <div>
+            <img src={footerLogo} alt="" />
+            <p className='text-[#CCC2C2] font-redRose text-[16px] tracking-[0.8px]'>The Shiba Inu is a Japanese breed of dog that was popularized as an online meme and represents Dogecoin. Dogecoin was created by Jackson Palmer & Shibetoshi Nakamoto.</p>
+          </div>
+
+        </div>
+      </Container>
     </div>
   );
 };
