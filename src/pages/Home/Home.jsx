fe12bd68b5a8f3df1530bb5dd110484d72156acb
Protap Ghosh <protapg518@gmail.com>
2024-03-10 21:24:57 +0600

    create development branch

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h2 className='font-poppins'>Home page content</h2>
+      <h2 className='font-poppins'>Home page poppins content</h2>
       <h2 className='font-redRose'>Home page content</h2>
     </div>
   );
