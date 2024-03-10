0358b9e4d1a79eface8cf3b898fcefcc1a6059c9
Protap Ghosh <protapg518@gmail.com>
2024-03-10 21:25:59 +0600

    added new branch

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <h2 className='font-poppins'>Home page poppins content</h2>
-      <h2 className='font-redRose'>Home page content</h2>
+      <h2 className='font-redRose'>Home page Red Rose content</h2>
     </div>
   );
 };
