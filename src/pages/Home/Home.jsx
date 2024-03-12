870d3dd074757d52c266810269b2c8cd2a1073f4
Protap Ghosh <protapg518@gmail.com>
2024-03-12 13:21:44 +0600

    added social media

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from '@/components/Sections/Banner/Banner';
 import FindOutMore from '@/components/Sections/FindOutMore/FindOutMore';
+import SocialMedia from '@/components/Sections/SocialMedia/SocialMedia';
 import WhatIsDogecoin from '@/components/Sections/WhatIsDogecoin/WhatIsDogecoin';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <SocialMedia />
       <WhatIsDogecoin />
       <FindOutMore />
     </div>
