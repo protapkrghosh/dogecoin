bae788b7cd771c35dae45a10b8e1ce77a5798b1f
Protap Ghosh <protapg518@gmail.com>
2024-03-12 23:05:53 +0600

    d

--- src/components/Sections/SocialMedia/SocialMedia.jsx
+++ src/components/Sections/SocialMedia/SocialMedia.jsx
@@ -9,9 +9,9 @@ const SocialMedia = () => {
     <div className='bg-[#F9748E] py-6 lg:py-7'>
       <Container>
         <div className='flex justify-center items-center'>
-          <img src={reddit} alt="Image" className='w-[90px] md:w-[120px] lg:w-[160px] h-auto'/>
-          <img src={discord} alt="Image" className='w-[120px] md:w-[150px] lg:w-[200px] h-auto mx-6 md:mx-14 lg:mx-36 2xl:mx-60'/>
-          <img src={instagram} alt="Image" className='w-[120px] md:w-[150px] lg:w-[200px] h-auto'/>
+          <img src={reddit} alt="Image" className='w-[90px] md:w-[120px] lg:w-[160px] h-auto cursor-pointer'/>
+          <img src={discord} alt="Image" className='w-[120px] md:w-[150px] lg:w-[200px] h-auto mx-6 md:mx-14 lg:mx-36 2xl:mx-60 cursor-pointer'/>
+          <img src={instagram} alt="Image" className='w-[120px] md:w-[150px] lg:w-[200px] h-auto cursor-pointer'/>
         </div>
       </Container>
     </div>
