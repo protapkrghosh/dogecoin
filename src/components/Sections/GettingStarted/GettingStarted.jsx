4fcf4474abcf48c58abe039f1ab0c8c23e37f224
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-14 00:33:54 +0600

    all lg section

--- src/components/Sections/GettingStarted/GettingStarted.jsx
+++ src/components/Sections/GettingStarted/GettingStarted.jsx
@@ -13,9 +13,9 @@ const GettingStarted = () => {
     <>
       <Container>
         <div className=''>
-          <div className='relative'>
+          <div className='lg:relative'>
             <p className='text-[#FFF] text-center text-[65px] font-bold font-redRose tracking-[5.85px] mt-[150px]'>Getting started</p>
-            <img className='absolute 2xl:ml-[400px] xl:ml-80 lg:ml-40 md:ml-16 ml-12' src={getImg} alt="" />
+            <img className='lg:absolute 2xl:ml-[400px] xl:ml-80 lg:ml-40 md:ml-16 ml-12' src={getImg} alt="" />
           </div>
           {/* getIcon  */}
           <div className='hidden lg:block'>
@@ -23,11 +23,11 @@ const GettingStarted = () => {
           </div>
 
           {/* icon card 1 */}
-          <div className='relative'>
-            <div className='absolute 2xl:mt-[-30px] 2xl:ml-[-20px] xl:mt-[-38px] xl:ml-[-30px] lg:mt-[-37px] lg:ml-[-35px]'>
+          <div className='lg:relative'>
+            <div className='lg:absolute 2xl:mt-[-30px] 2xl:ml-[-20px] xl:mt-[-38px] xl:ml-[-30px] lg:mt-[-37px] lg:ml-[-35px]'>
               <p className='2xl:text-[204px] xl:text-[204px] lg:text-[190px] font-bold font-poppins textBorderColor text-transparent'>1</p>
             </div>
-            <div className='2xl:w-[23%] xl:w-[28%] lg:w-[30%] absolute 2xl:mt-[35px] 2xl:ml-[-1px] xl:mt-[30px] xl:ml-[-15px] lg:mt-[30px] lg:ml-[-25px]'>
+            <div className='2xl:w-[23%] xl:w-[28%] lg:w-[30%] lg:absolute 2xl:mt-[35px] 2xl:ml-[-1px] xl:mt-[30px] xl:ml-[-15px] lg:mt-[30px] lg:ml-[-25px]'>
               <p className='text-[#FFF] text-center font-redRose text-[24px] tracking-[2.16px] mb-[15px]'>Choose your wallet</p>
               <p className='text-[#CCC2C2] text-center font-redRose text-[18px] tracking-[1.62px] mb-[30px]'>A wallet is necessary for people wanting to use, trade, or hold Dogecoin. You can pick a wallet</p>
               <div className='text-center'>
@@ -39,11 +39,11 @@ const GettingStarted = () => {
           </div>
 
           {/* icon card 2 */}
-          <div className='relative'>
-            <div className='absolute 2xl:mt-[-265px] 2xl:ml-[535px] xl:mt-[-222px] xl:ml-[410px] lg:mt-[-190px] lg:ml-[270px]'>
+          <div className='lg:relative'>
+            <div className='lg:absolute 2xl:mt-[-265px] 2xl:ml-[535px] xl:mt-[-222px] xl:ml-[410px] lg:mt-[-190px] lg:ml-[270px]'>
               <p className='2xl:text-[204px] xl:text-[204px] lg:text-[190px] font-bold font-poppins textBorderColor text-transparent'>2</p>
             </div>
-            <div className='2xl:w-[23%] xl:w-[25%] lg:w-[30%] absolute 2xl:mt-[-195px] 2xl:ml-[575px] xl:mt-[-155px] xl:ml-[460px] lg:mt-[-120px] lg:ml-[335px]'>
+            <div className='2xl:w-[23%] xl:w-[25%] lg:w-[30%] lg:absolute 2xl:mt-[-195px] 2xl:ml-[575px] xl:mt-[-155px] xl:ml-[460px] lg:mt-[-120px] lg:ml-[335px]'>
               <p className='text-[#FFF] text-center font-redRose text-[24px] tracking-[2.16px] mb-[15px]'>Choose your wallet</p>
               <p className='text-[#CCC2C2] text-center font-redRose text-[18px] tracking-[1.62px] mb-[30px]'>After downloading, you can configure your wallet according to our</p>
               <div className='text-center'>
@@ -55,11 +55,11 @@ const GettingStarted = () => {
           </div>
 
           {/*  icon card 3 */}
-          <div className='relative'>
-            <div className='absolute 2xl:mt-[-405px] 2xl:ml-[1050px] xl:mt-[-335px] xl:ml-[820px] lg:mt-[-270px] lg:ml-[615px]'>
+          <div className='lg:relative'>
+            <div className='lg:absolute 2xl:mt-[-405px] 2xl:ml-[1050px] xl:mt-[-335px] xl:ml-[820px] lg:mt-[-270px] lg:ml-[615px]'>
               <p className='2xl:text-[204px] xl:text-[204px] lg:text-[190px] font-bold font-poppins textBorderColor text-transparent'>3</p>
             </div>
-            <div className='2xl:w-[20%] xl:w-[25%] lg:w-[30%] absolute 2xl:mt-[-335px] 2xl:ml-[1100px] xl:mt-[-265px] xl:ml-[865px] lg:mt-[-205px] lg:ml-[658px]'>
+            <div className='2xl:w-[20%] xl:w-[25%] lg:w-[30%] lg:absolute 2xl:mt-[-335px] 2xl:ml-[1100px] xl:mt-[-265px] xl:ml-[865px] lg:mt-[-205px] lg:ml-[658px]'>
               <p className='text-[#FFF] text-center font-redRose text-[24px] tracking-[2.16px] mb-[15px]'>Get some dogecoin</p>
               <p className='text-[#CCC2C2] text-center font-redRose text-[18px] tracking-[1.62px] mb-[30px]'>There’s lots of ways to get your hands on some Dogecoin.</p>
               <div className='text-center'>
@@ -78,12 +78,12 @@ const GettingStarted = () => {
 
 
         <div className='mt-[450px] mb-[150px]'>
-          <div className='grid 2xl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 content-center gap-32 relative'>
+          <div className='grid 2xl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 content-center gap-32 lg:relative'>
             <img src={imgIcon1} alt="" />
             <img src={imgIcon2} alt="" />
             <img src={imgIcon3} alt="" />
             <img src={imgIcon4} alt="" />
-            <img className='absolute 2xl:w-[42px] xl:w-[34px] 2xl:ml-[105px] 2xl:mt-[142px] xl:ml-[86px] xl:mt-[118px] lg:ml-[86px] lg:mt-[118px] md:ml-[107px] md:mt-[147px] ml-[107px] mt-[147px]' src={imgIcon11} alt="" />
+            <img className='lg:absolute 2xl:w-[42px] xl:w-[34px] 2xl:ml-[105px] 2xl:mt-[142px] xl:ml-[86px] xl:mt-[118px] lg:ml-[86px] lg:mt-[118px] md:ml-[107px] md:mt-[147px] ml-[107px] mt-[147px]' src={imgIcon11} alt="" />
           </div>
         </div>
 
