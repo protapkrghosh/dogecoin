05ea458efd0c9441e7ca68949a94ffe42cc16a86
Protap Ghosh <protapg518@gmail.com>
2024-03-13 18:59:22 +0600

    responsive token card

--- src/components/Sections/StakeTokens/StakeTokens.jsx
+++ src/components/Sections/StakeTokens/StakeTokens.jsx
@@ -13,27 +13,27 @@ import { Button } from "@/components/ui/button";
 
 const StakeTokens = () => {
   return (
-    <div className="pt-28 pb-10">
+    <div className="pt-16 lg:pt-28 pb-10">
       <Container>
-        <div className="lg:flex items-center gap-x-10 lg:gap-x-14 xl:gap-x-24 text-center">
+        <div className="flex flex-col-reverse lg:flex-row items-center gap-x-10 lg:gap-x-14 xl:gap-x-24 text-center">
           {/* Buy now card */}
-          <div className="lg:w-2/5 text-white font-redRose bg-[#6F3232] border border-[#BB0F31] rounded-[28px] px-[30px] 2xl:px-[40px] py-[20px]">
-            <h3 className="text-[25px] font-semibold capitalize mb-5">Buy now before price rise</h3>
-            <div className="flex justify-between bg-[#BB0F31] rounded-[20px] px-[25px] 2xl:px-[30px] pt-[12px] pb-1">
+          <div className="md:w-2/3 lg:w-2/5 md:mx-auto lg:mx-0 text-white font-redRose bg-[#6F3232] border border-[#BB0F31] rounded-[28px] px-[30px] 2xl:px-[40px] py-[20px]">
+            <h3 className="text-[26px] lg:text-[22px] xl:text-[25px] font-semibold capitalize mb-5">Buy now before price rise</h3>
+            <div className="flex justify-between bg-[#BB0F31] rounded-[20px] px-5 md:px-[25px] lg:px-3 xl:px-[25px] 2xl:px-[30px] pt-[12px] pb-1">
               <div className="font-semibold">
-                <p className="text-[20px] -mb-2">Days</p>
+                <p className="text-[17px] md:text-[21px] lg:text-[18px] xl:text-[20px] -mb-2">Days</p>
                 <p className="text-[43px]">01</p>
               </div>
               <div className="font-semibold">
-                <p className="text-[20px] -mb-2">Hours</p>
+                <p className="text-[17px] md:text-[21px] lg:text-[18px] xl:text-[20px] -mb-2">Hours</p>
                 <p className="text-[43px]">02</p>
               </div>
               <div className="font-semibold">
-                <p className="text-[20px] -mb-2">Minutes</p>
+                <p className="text-[17px] md:text-[21px] lg:text-[18px] xl:text-[20px] -mb-2">Minutes</p>
                 <p className="text-[43px]">53</p>
               </div>
               <div className="font-semibold">
-                <p className="text-[20px] -mb-2">Second</p>
+                <p className="text-[17px] md:text-[21px] lg:text-[18px] xl:text-[20px] -mb-2">Second</p>
                 <p className="text-[43px]">02</p>
               </div>
             </div>
@@ -52,34 +52,34 @@ const StakeTokens = () => {
             </div>
 
             <div className="flex justify-center items-center my-4">
-              <img src={line} alt="Image" className="w-[50px]" />
-              <p className="uppercase tracking-[4px] px-2">1 $Poodl - $0.000445</p>
-              <img src={line} alt="Image" className="w-[50px]" />
+              <img src={line} alt="Image" className="w-[52px] md:w-[55px] lg:w-[50px]" />
+              <p className="uppercase tracking-[2px] md:tracking-[4px] lg:tracking-[0px] xl:tracking-[4px] px-2">1 $Poodl - $0.000445</p>
+              <img src={line} alt="Image" className="w-[52px] md:w-[55px] lg:w-[50px]" />
             </div>
 
             {/* Ethereum button */}
             <div className="flex justify-between gap-x-6">
-              <div className="flex items-center gap-3 2xl:gap-5 bg-[#BB0F31] w-fit px-4 py-[10px] rounded-[30px]">
-                <img src={icon1} alt="Image" className="w-[25px] 2xl:w-[31px]" />
-                <p className="text-[18px] text-start uppercase">ETH</p>
+              <div className="flex items-center gap-3 2xl:gap-5 bg-[#BB0F31] w-fit px-4 lg:px-2 xl:px-4 py-[10px] rounded-[30px]">
+                <img src={icon1} alt="Image" className="w-[22px] md:w-[31px] lg:w-[23px] xl:w-[25px] 2xl:w-[31px]" />
+                <p className="text-[17px] lg:text-[15px] xl:text-[18px] text-start uppercase mr-5 md:mr-0">ETH</p>
               </div>
 
-              <div className="flex items-center gap-3 2xl:gap-5 bg-[#BB0F31] w-fit px-4 py-[10px] rounded-[30px]">
-                <img src={icon2} alt="Image" className="w-[25px] 2xl:w-[31px]" />
-                <p className="text-[18px] text-start uppercase">ETH</p>
+              <div className="flex items-center gap-3 2xl:gap-5 bg-[#BB0F31] w-fit px-4 lg:px-2 xl:px-4 py-[10px] rounded-[30px]">
+                <img src={icon2} alt="Image" className="w-[22px] md:w-[31px] lg:w-[23px] xl:w-[25px] 2xl:w-[31px]" />
+                <p className="text-[17px] lg:text-[15px] xl:text-[18px] text-start uppercase mr-5 md:mr-0">ETH</p>
               </div>
 
-              <div className="flex items-center gap-3 2xl:gap-5 bg-[#BB0F31] w-fit px-4 py-[10px] rounded-[30px]">
-                <img src={icon3} alt="Image" className="w-[22px] 2xl:w-[28px]" />
-                <p className="text-[18px] text-start uppercase">ETH</p>
+              <div className="flex items-center gap-3 2xl:gap-5 bg-[#BB0F31] w-fit px-4 lg:px-2 xl:px-4 py-[10px] rounded-[30px]">
+                <img src={icon3} alt="Image" className="w-[22px] md:w-[28px] lg:w-[21px] xl:w-[22px] 2xl:w-[28px]" />
+                <p className="text-[17px] lg:text-[15px] xl:text-[18px] text-start uppercase mr-5 md:mr-0">ETH</p>
               </div>
             </div>
 
             {/* ETH you pay */}
             <div className="text-white flex justify-between mt-5 mb-4">
-              <p className="tracking-[2px] 2xl:tracking-[3px]">ETH you pay</p>
-              <p className="tracking-[2px] 2xl:tracking-[3px]">Max</p>
-              <p className="tracking-[2px] 2xl:tracking-[3px]">$Poodl you receive</p>
+              <p className="md:tracking-[2px] lg:tracking-[0px] xl:tracking-[2px] 2xl:tracking-[3px]">ETH you pay</p>
+              <p className="md:tracking-[2px] lg:tracking-[0px] xl:tracking-[2px] 2xl:tracking-[3px]">Max</p>
+              <p className="md:tracking-[2px] lg:tracking-[0px] xl:tracking-[2px] 2xl:tracking-[3px]">$Poodl you receive</p>
             </div>
 
             {/* Input field */}
@@ -98,12 +98,13 @@ const StakeTokens = () => {
             </div>
 
             {/* Button */}
-            <div className="flex justify-between gap-x-6 mt-7 mb-8">
-              <Button className="text-[15px] 2xl:text-[20px] font-semibold rounded-[30px] bg-[#6b2231] hover:bg-[#80283a] border border-[#BB0F31] py-6 2xl:py-7 tracking-[2px] 2xl:tracking-[3px] w-1/2">Connect Wallet</Button>
+            <div className="xl:flex justify-between gap-x-6 mt-7 mb-8">
+              <Button className="text-[18px] lg:text-[16px] xl:text-[15px] 2xl:text-[20px] font-semibold rounded-[30px] bg-[#6b2231] hover:bg-[#80283a] border border-[#BB0F31] py-6 2xl:py-7 tracking-[2px] 2xl:tracking-[3px] w-full xl:w-1/2">Connect Wallet</Button>
 
-              <div className="relative w-1/2">
-                <img src={icon4} alt="Image" className="w-[25px] absolute xl:top-[12px] 2xl:top-[14px] left-2 2xl:left-3 cursor-pointer" />
-                <Button className="text-[15px] 2xl:text-[20px] font-semibold rounded-[30px] bg-[#6b2231] hover:bg-[#80283a] border border-[#BB0F31] pl-10 2xl:pl-10 py-6 2xl:py-7 tracking-[2px] 2xl:tracking-[3px]">Buy with BNB</Button>
+              <div className="relative xl:w-1/2">
+                <img src={icon4} alt="Image" className="w-[25px] absolute top-8 xl:top-[12px] 2xl:top-[14px] left-16 md:left-24 lg:left-14 xl:left-2 2xl:left-3 cursor-pointer" />
+
+                <Button className="text-[18px] lg:text-[16px] xl:text-[15px] 2xl:text-[20px] font-semibold rounded-[30px] bg-[#6b2231] hover:bg-[#80283a] border border-[#BB0F31] pl-10 2xl:pl-10 py-6 2xl:py-7 mt-5 xl:mt-0 tracking-[2px] 2xl:tracking-[3px] w-full xl:w-auto">Buy with BNB</Button>
               </div>
             </div>
 
@@ -111,11 +112,11 @@ const StakeTokens = () => {
           </div>
 
           {/* Stake tokens */}
-          <div className="lg:w-auto font-redRose font-semibold">
+          <div className="lg:w-auto text-start font-redRose font-semibold">
             <h1 className="text-[#BB0F31] text-[30px] md:text-[38px] lg:text-[30px] xl:text-[40px] 2xl:text-[46px] uppercase">Stake $Dogecoin Tokens</h1>
-            <p className="text-[#eec4cc] text-[22px] tracking-[3px] text-start">Earn APY & The Earth</p>
+            <p className="text-[#eec4cc] text-[22px] tracking-[3px]">Earn APY & The Earth</p>
 
-            <img src={dogeImg} alt="Image" className="w-[420px] 2xl:w-[450px] mx-auto" />
+            <img src={dogeImg} alt="Image" className="lg:w-[420px] 2xl:w-[450px] mx-auto my-10 lg:mt-0" />
           </div>
         </div>
       </Container>
