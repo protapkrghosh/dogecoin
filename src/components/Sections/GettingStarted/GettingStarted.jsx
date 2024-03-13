1b28bd6e60eb8c7ef1dc8426cf4ef0165760f55f
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-13 19:50:54 +0600

    gettin sectip

--- src/components/Sections/GettingStarted/GettingStarted.jsx
+++ src/components/Sections/GettingStarted/GettingStarted.jsx
@@ -15,7 +15,7 @@ const GettingStarted = () => {
   return (
     <>
       <Container>
-        <div className='mb-[150px]'>
+        <div className='mb-[150px] mt-[-150px]'>
           <div className='relative'>
             <p className='text-[#FFF] text-center text-[65px] font-bold font-redRose tracking-[5.85px] mt-[150px]'>Getting started</p>
             <img className='absolute 2xl:ml-[400px] xl:ml-80 lg:ml-40 md:ml-16 ml-12' src={getImg} alt="" />
